a1fdb773d9918cebfa4c1066ce2dab6364710e7f
BroderFluff <jo.o.olsson@gmail.com>
2019-09-08 00:23:41 +0200

    a

--- main.c
+++ main.c
@@ -12,9 +12,16 @@ int main(int argc, char *argv[])
 
     struct pwm_channel *ch0 = PWM_export_channel(pwmc, 0);
 
-    PWM_channel_set_dutycycle(ch0, 1000000);
+    struct pwm_params params;
+    params.dutycycle = 1000000;
+    params.period = 1000000;
+    params.enable = 1;
+
+    PWM_update_channel(ch0, &params, PWM_DUTYCYCLE_BIT | PWM_PERIOD_BIT | PWM_ENABLE_BIT);
+
+    /*PWM_channel_set_dutycycle(ch0, 1000000);
     PWM_channel_set_period(ch0, 1000000);
-    PWM_channel_enable(ch0);
+    PWM_channel_enable(ch0);*/
 
 
     return 0;
