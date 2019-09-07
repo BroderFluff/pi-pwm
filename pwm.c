48dd75bd623c78e7a4cf45bc6d1b92ff3441e29d
BroderFluff <jo.o.olsson@gmail.com>
2019-09-07 17:18:07 +0200

    test

--- pwm.c
+++ pwm.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include <sys/types.h>
@@ -5,15 +6,40 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+struct pwm_chip
+{
+    const char *devicePath;
+    int npwm;
+};
+
+char buf[3];
+read(fd, buf, 3);
+puts(buf);
+
+int getNumPwmDevices(struct pwm_chip *pwm)
+{
+    if (pwm->npwm == -1) {
+        static char buf[3];
+        int fd = open("/sys/class/pwm/pwmchip0/npwm", O_RDONLY);
+
+        memset(buf, 0, sizeof(char) * 3);
+        read(fd, buf, 3);
+        pwm->npwm = atoi(buf);        
+    }
+
+    return pwm->npwm;
+}
+
 int main(int argc, char *argv[])
 {
     puts("Hej!");
 
-    int fd = open("/sys/class/pwm/pwmchip0/npwm", O_RDONLY);
+    struct pwm_chip pwmc;
 
-    char buf[3];
-    read(fd, buf, 3);
-    puts(buf);
+    int num = getNumPwmDevices(&pwmc);
+
+    printf("There are %d number of pwm channels", num);
+
 
     return 0;
 } 
