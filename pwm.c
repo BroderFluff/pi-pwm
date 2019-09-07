8d507e53986933bbf99f134d2933ec912b507ab9
BroderFluff <42545290+BroderFluff@users.noreply.github.com>
2019-09-07 16:59:40 +0200

    Create pwm.c

---
+++ pwm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hej!");
+
+    int fd = open("/sys/class/pwm/pwmchip0/npwm", O_RDONLY);
+
+    char buf[3];
+    read(fd, buf, 3);
+    puts(buf);
+
+    return 0;
+} 
